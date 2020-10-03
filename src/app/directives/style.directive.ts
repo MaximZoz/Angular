1b647bf75e6d59a82c5c404e6b86cc3bc28a3700
MaximZoz <max591018@gmail.com>
2020-10-03 11:11:16 +0200

    5. Как работает HostBinding

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -2,6 +2,7 @@ import { style } from '@angular/animations';
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
   Input,
   Renderer2,
@@ -19,31 +20,34 @@ export class StyleDirective {
     fontWeigth?: string;
     borderRadius?: string;
   };
+  @HostBinding('style.color') elColor = null;
 
   constructor(private el: ElementRef, private r: Renderer2) {}
   @HostListener('click', ['$event.target']) onClick(event: Event) {
     console.log(event);
   }
   @HostListener('mouseenter') onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', this.color);
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
-    this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
-    this.r.setStyle(
-      this.el.nativeElement,
-      'borderRadius',
-      this.dStyles.borderRadius
-    );
-    this.r.setStyle(
-      this.el.nativeElement,
-      'fontWeigth',
-      this.dStyles.fontWeigth
-    );
+    this.elColor = this.color;
+    // this.r.setStyle(this.el.nativeElement, 'color', this.color);
+    // this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
+    // this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
+    // this.r.setStyle(
+    //   this.el.nativeElement,
+    //   'borderRadius',
+    //   this.dStyles.borderRadius
+    // );
+    // this.r.setStyle(
+    //   this.el.nativeElement,
+    //   'fontWeigth',
+    //   this.dStyles.fontWeigth
+    // );
   }
   @HostListener('mouseleave') onLeave() {
-    this.r.setStyle(this.el.nativeElement, 'color', null);
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
-    this.r.setStyle(this.el.nativeElement, 'border', null);
-    this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
-    this.r.setStyle(this.el.nativeElement, 'fontWeigth', null);
+    this.elColor = null;
+    // this.r.setStyle(this.el.nativeElement, 'color', null);
+    // this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
+    // this.r.setStyle(this.el.nativeElement, 'border', null);
+    // this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
+    // this.r.setStyle(this.el.nativeElement, 'fontWeigth', null);
   }
 }
