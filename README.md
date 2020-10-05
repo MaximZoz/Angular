0cba4387d47fe2818d3fde3c7c106b8f30c432f6
MaximZoz <max591018@gmail.com>
2020-10-05 14:12:47 +0200

    7. Генерация пайпа с CLI

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
 
-## пайп процент, пайп даты, пайп для дебажа obj
-
-(pre) obj | json (pre) - дебажим переменную типа obj
+## Создаём свой пайп
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  float = 0.42;
-  obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: 4,
-      },
-    },
-  };
-}
+export class AppComponent {}
