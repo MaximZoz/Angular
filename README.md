ac514be1ee6fd9211778050fcce7affaf2940652
MaximZoz <max591018@gmail.com>
2020-10-11 14:19:03 +0200

    7. Модификация элементов с PUT

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
 
-## перенос логики из компонента в сервис
+## Оживляем кнопку (завершить) которая завершает определённый todo
 
-1. создаём сервис (src\app\todo.service.ts) - регистрируем, имплементируем его, импортируем в него функции, реализуем конструктор в нём с http
+1. в шаблоне создаём метод (completeTodo(todo.id))
+
+2. в todos.service реализуем метод completeTodo(todo.id) в нем this.http.put("ссылка" "compleate: true") "compleate: true" - поля, которые мы хотим модифицировать
+
+3. в appComponent обращаемся к todosService=>completeTodo и подписываемся на стрим (this.todosService.completeTodo(id).subscribe((todo: any) => {})
+
+4. в appComponent=> completeTodo() => subscribe() в todos с помощью метода find находим todo.id и присваеваем ему значение completed=true (this.todos.find(t=> t.id===todo.id).completed=true)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,58 @@
-import {Component, OnInit} from '@angular/core'
-import {HttpClient} from '@angular/common/http'
-import {delay} from 'rxjs/operators'
-import {Todo, TodosService} from './todos.service'
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs/operators';
+import { Todo, TodosService } from './todos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  todos: Todo[] = [];
 
-  todos: Todo[] = []
+  loading = false;
 
-  loading = false
-
-  todoTitle = ''
+  todoTitle = '';
 
   constructor(private todosService: TodosService) {}
 
   ngOnInit() {
-    this.fetchTodos()
+    this.fetchTodos();
   }
 
   addTodo() {
     if (!this.todoTitle.trim()) {
-      return
+      return;
     }
 
-    this.todosService.addTodo({
-      title: this.todoTitle,
-      completed: false
-    }).subscribe(todo => {
-      this.todos.push(todo)
-      this.todoTitle = ''
-    })
+    this.todosService
+      .addTodo({
+        title: this.todoTitle,
+        completed: false,
+      })
+      .subscribe((todo) => {
+        this.todos.push(todo);
+        this.todoTitle = '';
+      });
   }
 
   fetchTodos() {
-    this.loading = true
-    this.todosService.fetchTodos()
-      .subscribe(todos => {
-        this.todos = todos
-        this.loading = false
-      })
+    this.loading = true;
+    this.todosService.fetchTodos().subscribe((todos) => {
+      this.todos = todos;
+      this.loading = false;
+    });
   }
 
   removeTodo(id: number) {
-    this.todosService.removeTodo(id)
-      .subscribe(() => {
-        this.todos = this.todos.filter(t => t.id !== id)
-      })
+    this.todosService.removeTodo(id).subscribe(() => {
+      this.todos = this.todos.filter((t) => t.id !== id);
+    });
+  }
+  completeTodo(id: number) {
+    this.todosService.completeTodo(id).subscribe((todo: any) => {
+      this.todos.find((t) => t.id === todo.id).completed = true;
+    });
   }
 }
-
