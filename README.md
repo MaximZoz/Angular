491b7793ec424436a647eb8a6def89fa9fa72c0e
MaximZoz <max591018@gmail.com>
2020-10-12 15:03:20 +0200

    13. Модификация запросов и обработка ответов

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
 
-## Интерсептор (классы, которые позволяют перехватывать http запросы и что-то с ними делать)
+## Интерсептор (взаимодействуем с req и модифицируем его)
 
-1. создаём файл interceptor.ts, в нём export class AuthInterceptor implements HttpInterceptor
-
-2. Регистрируем его в appModule
-
-3. INTERCTPTOR_PROVIDER передаём в массив providers, чтобы его зарегистрировать
+1. в auth.interceptor req.clone передаём различные headers
+2. cloned передаём в return next.handle
--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,10 +1,12 @@
 import {
   HttpEvent,
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptor implements HttpInterceptor {
   intercept(
@@ -13,7 +15,16 @@ export class AuthInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     console.log('Intersept request', req);
 
-    return next.handle(req);
-    // throw new Error('Method not implemented.');
+    const cloned = req.clone({
+      headers: req.headers.append('Auth', 'Some random token'),
+    });
+
+    return next.handle(cloned).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Interceptor response', event);
+        }
+      })
+    );
   }
 }
