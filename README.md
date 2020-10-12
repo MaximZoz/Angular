d6de8a21ed67896ee992ef13f659706951189293
MaximZoz <max591018@gmail.com>
2020-10-12 12:22:05 +0200

    11. Параметр observe

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
 
-## Добавляем query параметры к запросам(query параметры - те элементы, которые идут после ? в ссылке)
+## Добавляем параметр observe к запросам(получаем детальный доступ до любого этапа запроса)
 
-1. todos.service => fetchTodos => get вторым параметром создаём query параметр (params: new HttpParams().set('\_limit', '3'))
-2. Если мы хотим создать несколько параметров, то надо закинуть их в пепременную let
+1. todos.service => fetchTodos => get вторым параметром создаём параметр observe
+
+2. в todos.service => fetchTodos => get.pipe(map()) создаём метод map() чтобы модифицировать данные
+
+3. в todos.service => removeTodo =>pipe создаём метод tap в котором создаём функцию ( if (event.type === HttpEventType.Sent) и (if (event.type === HttpEventType.Response)) которая позволяет нам следить за событиями до отправки на сервер и после
--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, delay } from 'rxjs/operators';
+import {
+  HttpClient,
+  HttpEventType,
+  HttpHeaders,
+  HttpParams,
+} from '@angular/common/http';
+import { observable, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, tap } from 'rxjs/operators';
 
 export interface Todo {
   completed: boolean;
@@ -36,9 +41,14 @@ export class TodosService {
     return this.http
       .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?', {
         params,
+        observe: 'response',
         // params: new HttpParams().set('_limit', '3')
       })
       .pipe(
+        map((response) => {
+          // console.log('TodosService -> constructor -> response', response);
+          return response.body;
+        }),
         delay(500),
         catchError((error) => {
           console.log(error.message);
@@ -47,16 +57,30 @@ export class TodosService {
       );
   }
 
-  removeTodo(id: number): Observable<void> {
-    return this.http.delete<void>(
-      `https://jsonplaceholder.typicode.com/todos/${id}`
-    );
+  removeTodo(id: number): Observable<any> {
+    return this.http
+      .delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        observe: 'events',
+      })
+      .pipe(
+        tap((event) => {
+          if (event.type === HttpEventType.Sent) {
+            console.log('Sent', event);
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log('Response', event);
+          }
+        })
+      );
   }
   completeTodo(id: number): Observable<Todo> {
     return this.http.put<Todo>(
       `https://jsonplaceholder.typicode.com/todos/${id}`,
       {
         completed: true,
+      },
+      {
+        responseType: 'json',
       }
     );
   }
