603539bdd260ff5858dca0ce1bad50659a13ed5e
MaximZoz <max591018@gmail>
2020-10-22 16:30:55 +0200

    8. Поиск динамических директив

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -1,32 +1,33 @@
-import { PostsComponent } from './posts.component';
-import { PostsService } from './posts.service';
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
-import { of } from 'rxjs';
+import {PostsComponent} from "./posts.component";
+import {PostsService} from "./posts.service";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {HttpClientModule} from "@angular/common/http";
+import {of} from "rxjs";
 
 describe('PostsComponent', () => {
-  let fixture: ComponentFixture<PostsComponent>;
-  let component: PostsComponent;
-  let service: PostsService;
+  let fixture: ComponentFixture<PostsComponent>
+  let component: PostsComponent
+  let service: PostsService
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PostsComponent],
-      providers: [PostsService],
-      imports: [HttpClientModule],
-    });
-    fixture = TestBed.createComponent(PostsComponent);
-    component = fixture.componentInstance;
+      declarations: [ PostsComponent ],
+      providers: [ PostsService ],
+      imports: [ HttpClientModule ]
+    })
 
-    // ! service = TestBed.get(PostsService);
-    service = fixture.debugElement.injector.get(PostsService);
-  });
+    fixture = TestBed.createComponent(PostsComponent)
+    component = fixture.componentInstance
+    // service = fixture.debugElement.injector.get(PostsService)
+    service = TestBed.get(PostsService)
+  })
 
   it('should fetch posts on ngOnInit', () => {
-    const posts = [1, '2', 3];
+    const posts = [1, 2, 3]
+    spyOn(service, 'fetch').and.returnValue(of(posts))
 
-    spyOn(service, 'fetch').and.returnValue(of(posts));
-    fixture.detectChanges();
-    expect(component.posts).toEqual(posts);
-  });
-});
+    fixture.detectChanges()
+
+    expect(component.posts).toEqual(posts)
+  })
+})
--- src/app/routing/routing.component.spec.ts
+++ src/app/routing/routing.component.spec.ts
@@ -1,20 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RoutingComponent } from './routing.component';
-import { Observable, Subject } from 'rxjs';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { ActivatedRoute, Params, Router, RouterOutlet } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 class RouterStub {
   navigate(path: string[]) {}
 }
+
 class ActivatedRouteStub {
   private subject = new Subject<Params>();
+
   push(params: Params) {
     this.subject.next(params);
   }
+
   get params() {
     return this.subject.asObservable();
   }
-  // params: Observable<Params>;
 }
 
 describe('RoutingComponent', () => {
@@ -24,11 +28,13 @@ describe('RoutingComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [RoutingComponent],
+      imports: [RouterTestingModule],
       providers: [
         { provide: Router, useClass: RouterStub },
         { provide: ActivatedRoute, useClass: ActivatedRouteStub },
       ],
     });
+
     fixture = TestBed.createComponent(RoutingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -37,17 +43,29 @@ describe('RoutingComponent', () => {
   it('should be defined', () => {
     expect(component).toBeDefined();
   });
-  it('should navigate to post if go back', () => {
-    let router = fixture.debugElement.injector.get(Router);
+
+  it('should navigate to posts if go back', () => {
+    let router = TestBed.get(Router);
     let spy = spyOn(router, 'navigate');
+
     component.goBack();
+
     expect(spy).toHaveBeenCalledWith(['/posts']);
   });
-  it('Should navigate to 404 if id =0', () => {
-    let router = fixture.debugElement.injector.get(Router);
-    let spy = spyOn(router, 'navigate');
+
+  it('should navigate to 404 if id = 0', () => {
+    let router = TestBed.get(Router);
     let route: ActivatedRouteStub = TestBed.get(ActivatedRoute);
+    let spy = spyOn(router, 'navigate');
+
     route.push({ id: '0' });
+
     expect(spy).toHaveBeenCalledWith(['/404']);
+  });
+
+  it('should have router-outlet directive', () => {
+    let de = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(de).not.toBeNull();
   });
 });
